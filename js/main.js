0b1a5f004b0ce7a5acc6d4b4def0efac40563ec8
vdloc <uvcudlco@gmail.com>
2023-02-12 21:07:11 +0700

    refactor header, add custom select styles

--- js/main.js
+++ js/main.js
@@ -1,26 +1,30 @@
 // SmartMenus init
-$(function() {
-  $('#main-menu').smartmenus({
+$(function () {
+  $("#main-menu").smartmenus({
     subMenusSubOffsetX: 6,
-    subMenusSubOffsetY: -8
+    subMenusSubOffsetY: -8,
   });
 });
 
 // SmartMenus mobile menu toggle button
-$(function() {
-  var $mainMenuState = $('#main-menu-state');
+$(function () {
+  var $mainMenuState = $("#main-menu-state");
   if ($mainMenuState.length) {
     // animate mobile menu
-    $mainMenuState.change(function(e) {
-      var $menu = $('#main-menu');
+    $mainMenuState.change(function (e) {
+      var $menu = $("#main-menu");
       if (this.checked) {
-        $menu.hide().slideDown(250, function() { $menu.css('display', ''); });
+        $menu.hide().slideDown(250, function () {
+          $menu.css("display", "");
+        });
       } else {
-        $menu.show().slideUp(250, function() { $menu.css('display', ''); });
+        $menu.show().slideUp(250, function () {
+          $menu.css("display", "");
+        });
       }
     });
     // hide mobile menu beforeunload
-    $(window).bind('beforeunload unload', function() {
+    $(window).bind("beforeunload unload", function () {
       if ($mainMenuState[0].checked) {
         $mainMenuState[0].click();
       }
@@ -32,22 +36,19 @@ $(function() {
 var btn_close_topmenu = document.querySelector("#main-menu-state");
 var main_topmenu = document.querySelector("#main-menu");
 btn_close_topmenu.onclick = function () {
-  if (!main_topmenu.classList.contains('show-mn')) {
-    main_topmenu.classList.add('show-mn');
+  if (!main_topmenu.classList.contains("show-mn")) {
+    main_topmenu.classList.add("show-mn");
   } else {
-    main_topmenu.classList.remove('show-mn');
-  };    
+    main_topmenu.classList.remove("show-mn");
+  }
 };
 
-// End Menu
-
-
-
+// Setup banner slider
 var swiper = new Swiper(".slide-main", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
-  },  
+  },
   autoHeight: true,
   autoplay: {
     delay: 25000,
@@ -55,33 +56,11 @@ var swiper = new Swiper(".slide-main", {
   },
 });
 
-// var swiper2 = new Swiper(".mySwipercard", {
-//   slidesPerView: 1,
-//   spaceBetween: 10,
-//   freeMode: true,
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-//   pagination: {
-//     el: ".mySwipercard .swiper-pagination",
-//     clickable: true,
-//   },
-//   breakpoints: {
-//     992: {
-//       slidesPerView: 4,
-//       spaceBetween: 30,
-//     },
-//     768: {
-//       slidesPerView: 3,
-//       spaceBetween: 20,
-//     },
-//     576: {
-//       slidesPerView: 2,
-//       spaceBetween: 15,
-//     },
-//   },
-// });
+// Setup languages selectbox customizations
+const languagesSelect = customSelect("#languages-select");
 
-// End slide
-
+// Scroll to top
+const scrollToTopButton = document.querySelector(".back-to-top");
+scrollToTopButton.addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
